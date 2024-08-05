8ed719c5c1aa0c2d711822d1b77d867ad7bddf17
KelvinTrinhSG <thkien85@Gmail.com>
2024-08-05 21:57:30 +0700

    Building Placement Rules
    Building Placement Rules

--- Assets/Scripts/BuildingTypeSelectUI.cs
+++ Assets/Scripts/BuildingTypeSelectUI.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class BuildingTypeSelectUI : MonoBehaviour
 {
     [SerializeField] private Sprite arrowSprite;
+    [SerializeField] private List<BuildingTypeSO> ignoreBuildingTypeList;
 
     private Dictionary<BuildingTypeSO, Transform> btnTransformDictionary;
 
@@ -40,6 +41,9 @@ public class BuildingTypeSelectUI : MonoBehaviour
 
 
         foreach (BuildingTypeSO buildingType in buildingTypeList.list) {
+
+            if (ignoreBuildingTypeList.Contains(buildingType)) continue;
+
             Transform btnTransform = Instantiate(btnTemplate, transform);
             btnTransform.gameObject.SetActive(true);
 
